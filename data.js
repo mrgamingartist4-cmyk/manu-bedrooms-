79187d66ff644873597bd8dd2ffde271cb6ba9e9
mrgamingartist4-cmyk <mrgamingartist4@gmail.com>
2026-09-21 12:00:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bed-1-",
+      "name": "bed 1 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.011724919800283118,
+        "pitch": 0,
+        "fov": 1.307717093362224
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bed-2",
+      "name": "bed 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-bed-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bed-3",
+      "name": "bed 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-bed-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bed-4",
+      "name": "bed 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.094021720985853,
+          "pitch": -0.04258405275100685,
+          "rotation": 0,
+          "target": "3-bed-4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
